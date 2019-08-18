f1862dcdfa13e7d575c57258980a9f84bc726390
ivelthoven <ivo@ivelthoven.nl>
2019-08-18 15:43:20 +0200

    initial commit

---
+++ js/config.js
@@ -0,0 +1,55 @@
+// ignore
+var Config = {};
+
+/**
+ * LOADING SCREEN MADE BY Exsite
+ * Feel free to use, just don't ask money enc for it..
+ */
+
+/**
+ * What should the text in the center of the screen be?
+ * if empty it will fill in your Server Name
+ */
+Config.title = "SigmaRP";
+
+/**
+ * Enable map text in the top left corner of the screen?
+ */
+Config.enableMap = true;
+
+/**
+ * Enable steamId text in the top right corner of the screen?
+ */
+Config.enableSteamID = true;
+
+/**
+ * Enable announcements?
+ */
+Config.enableAnnouncements = true;
+
+/**
+ * What messages do you want to show up?
+ * only works if enableAnnouncements = true
+ */
+Config.announceMessages = [
+  "Enjoy your stay!",
+  "Please consider to join our discord",
+  "Loading screen made by Exsite"
+];
+
+/**
+ * How many miliseconds for each announcement?
+ * only works if enableAnnouncements = true
+ */
+Config.announcementLength = 3000;
+
+/**
+ * Image Filename
+ * DROP IMAGE IN "images" FOLDER
+ */
+Config.backgroundImage = "space.jpg";
+
+/**
+ * Enable debug messages?
+ */
+Config.enableDebug = false;
